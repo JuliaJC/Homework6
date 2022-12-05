9ee7e8d498d89cb98ec68b006e0ba78dee3c66c7
Julia <julso@LAPTOP-J5CQF40T.(none)>
2022-12-05 19:18:21 +0300

    Zadacha41 added

---
+++ Zadacha41/Program.cs
@@ -0,0 +1,65 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 4*/
+
+string inputNumbers = ReadString("Введите любое количество чисел через запятую: ");
+char split = ','; 
+
+WriteArray(ParsArray(inputNumbers, split));
+int count = 0;
+for(int i = 0; i < inputNumbers.Length; i++)
+{
+    if(inputNumbers[i]>0)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine(count);
+
+int[] ParsArray(string inputNumbers, char split)
+{
+    int numbersCount = 1;
+    for(int i = 0; i < inputNumbers.Length; i++)
+    {
+        if(inputNumbers[i] == split)
+        {
+            numbersCount++;
+        }
+    }
+
+    int[] numbers = new int[numbersCount];
+    int numberIndex = 0;
+    string subString = "";
+
+    for(int i = 0; i < inputNumbers.Length; i++)
+    {
+        if(inputNumbers[i] == split)
+        {
+            numbers[numberIndex++] = Convert.ToInt32(subString);
+            subString = "";
+        }
+        else
+        {
+            subString += inputNumbers[i];
+        }
+    }
+    numbers[numberIndex] = Convert.ToInt32(subString);
+    return numbers;
+}
+
+string? ReadString(string message)
+{
+    Console.Write(message);
+    return Console.ReadLine();
+}
+
+void WriteArray(int[] array)
+{
+for(int i = 0; i < array.Length; i++)
+{
+Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+}
